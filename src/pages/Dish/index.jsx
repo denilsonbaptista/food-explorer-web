03cfbd4d6de4a421385d902bc1b773e4f14cf103
denilsonbaptista <baptistadenilsonbp@gmail.com>
2024-05-05 19:06:39 -0300

    feat: consume api data to the dish page

--- src/pages/Dish/index.jsx
+++ src/pages/Dish/index.jsx
@@ -1,12 +1,36 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import { CaretLeft } from '@phosphor-icons/react'
+
+import { api } from '../../services/api'
 
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
 import { Tag } from '../../components/Tag'
-import { Container, Details, Descriptions, Ingredient, EditDish } from './styles'
+import {
+  Container,
+  Details,
+  Descriptions,
+  Ingredient,
+  EditDish,
+} from './styles'
 
 export function Dish() {
+  const [data, setData] = useState('')
+
+  const params = useParams()
+
+  const imgUrl = `${api.defaults.baseURL}/files/${data.image_url}`
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await api.get(`/foods/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <Container>
       <Header />
@@ -18,23 +42,21 @@ export function Dish() {
         </Link>
 
         <Details>
-          <img src="http://localhost:3333/files/e26d12370cc18d86bb34-image%203.png" alt="" />
+          <img src={imgUrl} alt="" />
 
           <Descriptions>
-            <h2>Salada Ravanello</h2>
+            <h2>{data.name}</h2>
+            <p>{data.description}</p>
 
-            <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O pão naan dá um toque especial.</p>
+            {data.ingredients && (
+              <Ingredient>
+                {data.ingredients.map(ingredient => (
+                  <Tag title={ingredient.name} key={ingredient.id} />
+                ))}
+              </Ingredient>
+            )}
 
-            <Ingredient>
-              <Tag title="alface" />
-              <Tag title="cebole" />
-              <Tag title="pão naan" />
-              <Tag title="pepino" />
-              <Tag title="rabanete" />
-              <Tag title="tomate" />
-            </Ingredient>
-
-            <EditDish to="/edit/5">Editar prato</EditDish>
+            <EditDish to={`/edit/${params.id}`}>Editar prato</EditDish>
           </Descriptions>
         </Details>
       </div>
